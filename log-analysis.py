75bbd5ff17870f44a3d94184aac9cbd2afdd65cc
brusznicki <chris.brusznicki@gmail.com>
2017-08-25 14:11:25 -0500

    fixed a few bugs added better styling

--- log-analysis.py
+++ log-analysis.py
@@ -16,11 +16,11 @@ class LogAnalysis():
 
         questions = self.get_questions()
 
-        for question in sorted(questions):
+        for key in sorted(questions.keys()):
             self.print_result(
-                question['question'],
-                self.execute_query(question['query']),
-                question['label'])
+                questions[key]['question'],
+                self.execute_query(questions[key]['query']),
+                questions[key]['label'])
 
     def get_questions(self):
         '''Contains a list of user provided questions and queries
@@ -28,50 +28,50 @@ class LogAnalysis():
         questions = {}  # Hash of questions and associated queries
 
         # LOG ANALYSIS QUESTION 1
-        question1 = """
-            1. What are the most popular three articles of all time?
+        question1 = """1. What are the most popular three articles of all time?
         """
 
         query1 = """
-            SELECT a.title, count(l.path) AS views
+            SELECT a.title, TO_CHAR(views, 'FM999,999,999')
                 FROM articles a
-                    LEFT JOIN log l
-                    ON
-                    a.slug = REPLACE(l.path, '/article/', '')
-                WHERE l.status = '200 OK'
-                GROUP BY a.title, l.path
+                    LEFT JOIN (SELECT path, count(log.path) AS views
+                               FROM log
+                               WHERE status = '200 OK'
+                               GROUP BY PATH) AS l
+                    ON REPLACE(l.path, '/article/', '') LIKE a.slug
                 ORDER BY views DESC
                 LIMIT 3;
         """
-        questions[1]['question'] = question1
-        questions[1]['query'] = query1
-        questions[1]['label'] = ' views'
+        questions[1] = {'question': question1,
+                        'query': query1,
+                        'label': ' views'}
 
         # LOG ANALYSIS QUESTION 2
-        question2 = """
-            2. Who are the most popular article authors of all time?
+        question2 = """2. Who are the most popular article authors of all time?
         """
         query2 = """
-            SELECT authors.name, count(log.path) AS views
-                FROM authors, articles, log
-                WHERE authors.id = articles.author
-                    AND articles.slug = REPLACE(log.path, '/article/', '')
+            SELECT authors.name, TO_CHAR(sum(views), 'FM999,999,999')
+                FROM articles
+                    JOIN authors on articles.author = authors.id
+                        LEFT JOIN (SELECT path, count(*) AS views
+                                   FROM log
+                                   WHERE status = '200 OK'
+                                   GROUP BY PATH) AS l
+                        ON REPLACE(l.path, '/article/', '')
+                        LIKE articles.slug
                 GROUP BY authors.name
-                ORDER BY views DESC;
+                ORDER BY SUM(views) DESC;
         """
-        questions[2]['question'] = question2
-        questions[2]['query'] = query2
-        questions[2]['label'] = ' views'
+        questions[2] = {'question': question2,
+                        'query': query2,
+                        'label': ' views'}
 
         # LOG ANALYSIS QUESTION 3
-        question3 = """
-            '3. On which days did more than 1% of requests lead to errors?'
+        question3 = """3. On which days did more than 1% of requests lead to errors?'
         """
         query3 = """
-            SELECT date_trunc('day', day),
-                   success,
-                   fail,
-                   cast(fail AS decimal(12,2)) / (fail + success)
+            SELECT TO_CHAR(date_trunc('day', day), 'FMMonth DD, YYYY'),
+                   round(fail*100.0 / (fail + success),2)
                    AS quotient
                 FROM (
                     SELECT
@@ -87,9 +87,11 @@ class LogAnalysis():
                 WHERE cast(fail AS decimal(12,2)) / (fail + success) > 0.01
                 GROUP BY date_trunc('day', day), success, fail;
         """
-        questions[3]['question'] = question3
-        questions[3]['query'] = query3
-        questions[3]['label'] = '% errors'
+        questions[3] = {'question': question3,
+                        'query': query3,
+                        'label': '% errors'}
+
+        return questions
 
     def execute_query(self, query):
         """Executes query, returns result"""
@@ -107,7 +109,7 @@ class LogAnalysis():
     def db_connect(self, db_name):
         """Connect to database, db_name, return connection """
         try:
-            db = psycopg2.connect('database={}'.format(db_name))
+            db = psycopg2.connect("dbname={}".format(db_name))
             return db
         except psycopg2.Error as e:
             print "Unable to connect to {}, check your connection"\
@@ -115,15 +117,14 @@ class LogAnalysis():
             return None
 
     def print_result(self, question, result, label):
-        '''Produces a readable output'''
-        print ""
-        print "-" * 50
+        '''Produces text only output of query result'''
+        print "\n"
+        print "*" * len(question)
         print question
-        print "-" * 50
+        print "*" * len(question)
         for row in result:
             print " -- ".join([str(x) for x in row]) + label
-        print "=" * 50
-        print ""
+        print "=" * len(question)
 
 
 if __name__ == '__main__':
